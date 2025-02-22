70a30bc5de6b5b9a2453d1ce1ce562e76ee136a2
Sashank Gogula <sashank.gogula@glean.com>
2025-02-22 10:20:08 -0800

    add project id

--- basic.config.ts
+++ basic.config.ts
@@ -3,16 +3,17 @@
 // docs: https://docs.basic.tech/info/schema 
 
 export const schema = {
-  project_id: '',
-  version: 0,
-  tables: {
-    emojis: {
-      type: 'collection',
-      fields: {
-        value: {
-          type: 'string',
-        },
-      },
-    },
+  "project_id": "c64278ad-7e9f-4fed-bf14-5af221796833",
+  "tables": {
+    "location": {
+      "type": "collection",
+      "fields": {
+        "coordinates": {
+          "type": "string",
+          "indexed": true
+        }
+      }
+    }
   },
+  version: 1,
 }
